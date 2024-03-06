af0cfd5abaadeb2d64fe0f5c442548a857aee316
SebastianCamacho <jscamacho@unicesar.edu.co>
2024-03-05 19:30:15 -0500

    creada carpeta modelo y servicio

---
+++ FrontendAngular/src/app/models/persona.ts
@@ -0,0 +1,12 @@
+export class Persona {
+    identificacion: string;
+    nombre: string;
+    apellido:string;
+    sexo: string;
+    edad: number;
+    departamento:string;
+    ciudad:string;
+    valorDeApoyo:number;
+    modalidad:string;
+    fecha:string;
+}
