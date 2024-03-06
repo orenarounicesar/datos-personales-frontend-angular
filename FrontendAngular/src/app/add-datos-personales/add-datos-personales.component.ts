532d171688a07849bf5e42c4abd07ba391ebfefc
andres yaruro <afyaruro20@unicesar.edu.co>
2024-03-05 22:13:32 -0500

    enrutado

--- FrontendAngular/src/app/add-datos-personales/add-datos-personales.component.ts
+++ FrontendAngular/src/app/add-datos-personales/add-datos-personales.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-datos-personales',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './add-datos-personales.component.html',
   styleUrl: './add-datos-personales.component.css'
 })
 export class AddDatosPersonalesComponent {
+
 
 }
--- FrontendAngular/src/app/app.routes.ts
+++ FrontendAngular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ActualizarDocumentoComponent }  from './actualizar-documento/actualizar-documento.component';
+import { AddDatosPersonalesComponent } from './add-datos-personales/add-datos-personales.component';
 
 export const routes: Routes = [
-    {path: 'actualizar_routes', component: ActualizarDocumentoComponent }
-    
+    {path: 'actualizar_routes', component: ActualizarDocumentoComponent },
+    {path: "add", component: AddDatosPersonalesComponent},
+    {path: "", redirectTo: "add", pathMatch: "full"}
 ];
 
 
