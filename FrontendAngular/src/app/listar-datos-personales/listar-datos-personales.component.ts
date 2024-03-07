e12b189cb3278fe369ab4eb7be6275ca00afb781
SebastianCamacho <jscamacho@unicesar.edu.co>
2024-03-07 08:51:45 -0500

    adicion de vits listar

---
+++ FrontendAngular/src/app/listar-datos-personales/listar-datos-personales.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-datos-personales',
+  standalone: true,
+  imports: [],
+  templateUrl: './listar-datos-personales.component.html',
+  styles: ``
+})
+export class ListarDatosPersonalesComponent {
+
+}
